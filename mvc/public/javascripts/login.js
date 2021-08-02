545afaef9f050662fff592f5230349d9ce96bb68
mnatour2 <55928250+mnatour2@users.noreply.github.com>
2021-08-02 17:12:07 +0300

    add validation with jQuery

--- mvc/public/javascripts/login.js
+++ mvc/public/javascripts/login.js
@@ -1,22 +1,47 @@
-const loginForm = $("form#login-form");
-
-loginForm.on("submit", function (e) {
-  e.preventDefault();
-
-  const error_message = $("#usernamePasswordWrong2");
-  if (error_message) error_message.addClass("d-none");
-  const isUsernameValid = validateFieldLogin(
-    "floatingInput",
-    loginRegex,
-    "usernamePasswordWrong"
-  );
-  const isPasswordValid = validateFieldLogin(
-    "floatingPassword",
-    loginRegex,
-    "usernamePasswordWrong"
-  );
-
-  if (isUsernameValid && isPasswordValid) {
-    this.submit();
-  }
+$().ready(function () {
+  $("form#login-form").validate({
+    rules: {
+      username: {
+        required: true,
+        minlength: 2,
+      },
+      password: {
+        required: true,
+        minlength: 8,
+      },
+    },
+    messages: {
+      username: {
+        required: "Please enter a username",
+        minlength: "wrong username or password",
+      },
+      password: {
+        required: "Please enter a password",
+        minlength: "invalid username or password",
+      },
+    },
+  });
 });
+
+// const loginForm = $("form#login-form");
+
+// loginForm.on("submit", function (e) {
+//   e.preventDefault();
+
+//   const error_message = $("#usernamePasswordWrong2");
+//   if (error_message) error_message.addClass("d-none");
+//   const isUsernameValid = validateFieldLogin(
+//     "floatingInput",
+//     loginRegex,
+//     "usernamePasswordWrong"
+//   );
+//   const isPasswordValid = validateFieldLogin(
+//     "floatingPassword",
+//     loginRegex,
+//     "usernamePasswordWrong"
+//   );
+
+//   if (isUsernameValid && isPasswordValid) {
+//     this.submit();
+//   }
+// });
--- mvc/public/javascripts/register.js
+++ mvc/public/javascripts/register.js
@@ -1,44 +1,96 @@
-const registerForm = $("form#register-form");
-
-registerForm.on("submit", function (e) {
-  e.preventDefault();
-  const isUsernameValid = validateField(
-    "validationTooltipUsername",
-    usernameRegex,
-    "usernameErrorMessage"
-  );
-  const isEmailValid = validateField(
-    "validationTooltip01",
-    emailRegex,
-    "emailErrorMessage"
-  );
-  const isPasswordValid = validateField(
-    "validationTooltip02",
-    passwordRegex,
-    "passwordErrorMessage"
-  );
-  const isConfirmPasswordValid = checkConfirmPassword(
-    $("#validationTooltip02"),
-    $("#validationTooltip03"),
-    "confrimPasswordErrorMessage"
-  );
-  const isMobileValid = validateField(
-    "validationTooltip04",
-    mobileRegex,
-    "mobileErrorMessage"
-  );
-
-  if (
-    isUsernameValid &&
-    isEmailValid &&
-    isPasswordValid &&
-    isConfirmPasswordValid &&
-    isMobileValid
-  ) {
-    $("#validationTooltip01").val(
-      $("#validationTooltip01").val() + "@vatrin.com"
-    );
-    $("#validationTooltip04").val("05" + $("#validationTooltip04").val());
-    this.submit();
-  }
+$().ready(function () {
+  $("form#register-form").validate({
+    rules: {
+      username: {
+        required: true,
+        minlength: 10,
+        maxlength: 30,
+      },
+      email: {
+        required: true,
+      },
+      password: {
+        required: true,
+        minlength: 8,
+      },
+      confirmPassword: {
+        required: true,
+        minlength: 8,
+        equalTo: "#validationTooltip02",
+      },
+      mobile: {
+        required: true,
+        minlength: 8,
+        maxlength: 8,
+      },
+    },
+    messages: {
+      username: {
+        required: "Please enter a username",
+        minlength: "Invalid username",
+      },
+      email: {
+        required: "Please enter a email",
+      },
+      password: {
+        required: "Please enter a password",
+        minlength: "Invalid password",
+      },
+      confirmPassword: {
+        required: "Please confirm the password",
+        minlength: "Invalid password",
+        equalTo: "Please match the passwords",
+      },
+      mobile: {
+        required: "Please enter a password",
+        minlength: "Invalid mobile",
+        maxlength: "Invalid mobile",
+      },
+    },
+  });
 });
+
+// const registerForm = $("form#register-form");
+
+// registerForm.on("submit", function (e) {
+//   e.preventDefault();
+//   const isUsernameValid = validateField(
+//     "validationTooltipUsername",
+//     usernameRegex,
+//     "usernameErrorMessage"
+//   );
+//   const isEmailValid = validateField(
+//     "validationTooltip01",
+//     emailRegex,
+//     "emailErrorMessage"
+//   );
+//   const isPasswordValid = validateField(
+//     "validationTooltip02",
+//     passwordRegex,
+//     "passwordErrorMessage"
+//   );
+//   const isConfirmPasswordValid = checkConfirmPassword(
+//     $("#validationTooltip02"),
+//     $("#validationTooltip03"),
+//     "confrimPasswordErrorMessage"
+//   );
+//   const isMobileValid = validateField(
+//     "validationTooltip04",
+//     mobileRegex,
+//     "mobileErrorMessage"
+//   );
+
+//   if (
+//     isUsernameValid &&
+//     isEmailValid &&
+//     isPasswordValid &&
+//     isConfirmPasswordValid &&
+//     isMobileValid
+//   ) {
+//     $("#validationTooltip01").val(
+//       $("#validationTooltip01").val() + "@vatrin.com"
+//     );
+//     $("#validationTooltip04").val("05" + $("#validationTooltip04").val());
+//     this.submit();
+//   }
+// });
